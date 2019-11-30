e955eaf8250c0f98f719189d960a365af9b24a09
nagasankar <nagasankar24@gmail.com>
2019-11-30 11:47:27 +0530

    this new file chnged version

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("***********************");  
+     System.out.println("   hello     linux     ");  
      System.out.println("     HELLO DEVOPS      ");  
      System.out.println("***********************");  
     }  
