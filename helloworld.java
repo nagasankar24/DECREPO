8efe07e4541957857f7685546e07860de3ff614a
nagasankar <nagasankar24@gmail.com>
2019-11-30 12:47:59 +0530

    this second time edited file

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("   hello     linux     ");  
      System.out.println("     HELLO DEVOPS      ");  
-     System.out.println("***********************");  
+     System.out.println(".......................");  
     }  
 } 
