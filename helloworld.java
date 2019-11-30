32935e5682123988cd96f9f634d4b624b2999bc8
nagasankar <nagasankar24@gmail.com>
2019-11-30 11:40:55 +0530

    changed java file

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("-----------------------");  
+     System.out.println("***********************");  
      System.out.println("     HELLO DEVOPS      ");  
-     System.out.println("-----------------------");  
+     System.out.println("***********************");  
     }  
 } 
